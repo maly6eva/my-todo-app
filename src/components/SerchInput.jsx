59459f7553d965b702891384e3c1f0204fe57bc0
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2025-04-25 12:41:11 +0300

    first commit

---
+++ src/components/SerchInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SerchInput = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const TaskList = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
